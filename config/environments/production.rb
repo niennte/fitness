7f760adc7aeba3a02f41aa7e055629032aff5a41
Niennte <niennte@gmail.com>
2018-07-08 18:17:33 -0400

    Add mail configuration.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,17 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: 'powerful-stream-48514.herokuapp.com/', port: 25 }
+  config.action_mailer.default_url_options = { host: 'powerful-stream-48514.herokuapp.com/' }
+
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@powerful-stream-48514.herokuapp.com'}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
