15d26b4cc1f0dc5c7d905748e5fd2d52059ece9e
Niennte <niennte@gmail.com>
2018-07-23 14:00:02 -0400

    Add integration tests.

---
+++ test/integration/user_workflows.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class UserWorkflowsTest < ActionDispatch::IntegrationTest
+
+  fixtures :activities, :users
+
+  setup do
+    @user = users(:one)
+  end
+
+  test 'user can sign in and gets redirected to original request' do
+
+    post user_session_path, params: { user: { email: @user.email, password: 'qwerty1' } }
+    assert_response :redirect
+  end
+
+  test 'after signing in, user can create activity' do
+    post user_session_path, params: { user: { email: @user.email, password: 'qwerty1' } }
+    assert_response :redirect
+
+    assert_difference('Activity.count') do
+      post activities_url, params: {
+        activity:
+          {
+            activity_date: Date.current,
+            activity_type: 'running',
+            hours: 1,
+            minutes: 30
+          }
+      }
+    end
+    assert_redirected_to activity_url(Activity.last)
+  end
+
+  test 'after signing in, user can edit their activity' do
+    post user_session_path, params: { user: { email: @user.email, password: 'qwerty1' } }
+    assert_response :redirect
+
+    get edit_activity_path(@user.activities.first)
+    assert_response :success
+    patch activity_path(@user.activities.first), params: { activity: { activity_type: 'pint_lifting'} }
+    assert_equal 'pint_lifting', @user.activities.first.activity_type
+    assert_redirected_to activity_url(@user.activities.first)
+    assert_redirected_to activity_url(Activity.first)
+  end
+
+  test 'after signing in, user can delete their activity' do
+    post user_session_path, params: { user: { email: @user.email, password: 'qwerty1' } }
+    assert_response :redirect
+    assert_difference('Activity.count', -1) do
+      delete activity_url(@user.activities.first)
+    end
+  end
+
+
+
+
+end
