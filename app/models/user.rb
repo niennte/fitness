fc31c9a4f604cdf952be3047de0041b6b99dbb51
Niennte <niennte@gmail.com>
2018-07-23 14:00:34 -0400

    Fix inconsistency in model relationships.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :timeoutable, :validatable, :lockable
+
+  has_many :activities
+
 end
